2ca5ad37789803e66c2e5b151532252e7b1080aa
Makarand Milind Hinge <mmh2212002@gmail.com>
2026-01-23 09:57:28 +0530

    generix is on

---
+++ src/main/java/generic/Main.java
@@ -0,0 +1,50 @@
+package generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+//        Box<Integer> box = new Box<>();
+//        box.setValue(20);
+//
+//        Box<String> box2 = new Box<>();
+//        box2.setValue("Hello");
+//
+//        System.out.println(box.getValue());
+//        System.out.println(box2.getValue());
+//        box2.setValue(10);
+
+//        Integer[] nums = {1,2,3};
+//        String[] names = {"A","B"};
+//
+//        Util.printArray(nums);
+//        Util.printArray(names);
+
+//        System.out.println(Util.max(10,20));
+//        System.out.println(Util.max("apple","banana"));
+
+//        System.out.println(Util.max(new Car(),new Car()));
+
+//        List<String> list1 = new ArrayList<>();
+//        List<Integer> list2 = new ArrayList<>();
+//
+//        System.out.println(list1.getClass());
+//        System.out.println(list2.getClass());
+
+//        List raw = new ArrayList();
+//        raw.add("Hello");
+//
+//        List<String> typed = raw;   // unchecked warning
+//        String s = typed.get(0);
+
+
+        }
+
+}
